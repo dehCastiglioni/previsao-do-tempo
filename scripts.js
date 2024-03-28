97d9cf89e3270f376a3ac1b805c9229a407807e7
André <de.castiglioni@gmail.com>
2024-03-28 13:11:18 -0300

    update code

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function outputData(dados) {
     document.querySelector(".cidade").innerHTML = "Previsão para " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
